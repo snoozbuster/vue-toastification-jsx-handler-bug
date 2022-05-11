8de6c5ec02516b579289b24d8c9fcd666b277c6d
Alex Van Liew <alexv@productiv.com>
2022-05-11 01:49:03 -0700

    reproduce issue

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,43 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <br>
+    <button @click="openGoodToast">Click me for a toast with a working button</button>
+    <button @click="openBadToast">Click me for a toast with a broken button</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  methods: {
+    getJsx() {
+      return (
+        <button onClick={() => alert('ok!')}>
+          Click me!
+        </button>
+      )
+    },
+    openGoodToast() {
+      const jsx = this.getJsx()
+      this.$toast.success({ render: () => jsx })
+    },
+    openBadToast() {
+      this.$toast.error(this.getJsx())
+    },
+  },
 }
 </script>
 
-<style>
+<style lang="scss">
+@import "vue-toastification/src/scss/_variables";
+@import "vue-toastification/src/scss/_toastContainer";
+@import "vue-toastification/src/scss/_toast";
+@import "vue-toastification/src/scss/_closeButton";
+@import "vue-toastification/src/scss/_progressBar";
+@import "vue-toastification/src/scss/_icon";
+@import "vue-toastification/src/scss/animations/_bounce";
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
+import VueToastificationPlugin from 'vue-toastification'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+Vue.use(VueToastificationPlugin, { closeOnClick: false });
 
 new Vue({
   render: h => h(App),
